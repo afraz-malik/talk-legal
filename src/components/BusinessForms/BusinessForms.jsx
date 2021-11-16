47a76341b520bc91d4e2e17610db855c2dcf9a7e
Afraz Malik <afrazmalik321@gmail.com>
2021-11-16 18:10:31 +0500

    'updates'

--- src/components/BusinessForms/BusinessForms.jsx
+++ src/components/BusinessForms/BusinessForms.jsx
@@ -1,30 +1,10 @@
 import React from 'react'
 import BFormsCss from './BusinessForms.module.scss'
-import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router'
-import { clearForm, gettingFormStart } from '../../redux/data/data.action'
-import { currentFormSelector } from '../../redux/data/data.selector'
 
 const BusinessForms = ({ forms }) => {
-  const currentForm = useSelector((state) => currentFormSelector(state))
-  const dispatch = useDispatch()
   const history = useHistory()
-  console.log(currentForm)
-  const getForm = (id) => {
-    if (currentForm) {
-      if (currentForm.id === id) {
-        history.push({ pathname: '/businessform', form: id })
-      } else {
-        dispatch(clearForm())
-        dispatch(gettingFormStart(id))
-        history.push({ pathname: '/businessform', form: id })
-      }
-    } else {
-      dispatch(clearForm())
-      dispatch(gettingFormStart(id))
-      history.push({ pathname: '/businessform', form: id })
-    }
-  }
+
   return (
     <div className={BFormsCss.container}>
       <div className={BFormsCss.helper}></div>
