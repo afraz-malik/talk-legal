2515edcd39eb363dc60869e212f593d1e38a6feb
Afraz Malik <afrazmalik321@gmail.com>
2021-11-19 18:52:08 +0500

    updats

--- front-end/src/components/DashboardBoxmodel/DashboardBoxmodel.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const DashboardBoxmodel = () => {
-  return <div></div>
-}
-
-export default DashboardBoxmodel
--- front-end/src/components/Preview/Preview.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import PreviewCss from './Preview.module.scss'
-const Preview = ({ children }) => {
-  return <div className={PreviewCss.preview}>{children}</div>
-}
-
-export default Preview
