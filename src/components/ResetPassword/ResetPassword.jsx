9f590c488ba55410d49d13034ef6092025382d0a
Afraz Malik <afrazmalik321@gmail.com>
2021-12-01 19:13:05 +0500

    'dupates'

--- src/components/ResetPassword/ResetPassword.jsx
+++ src/components/ResetPassword/ResetPassword.jsx
@@ -1,61 +1,60 @@
-import React from "react";
-import ResetPasswordCss from "./ResetPassword.module.scss";
+import React from 'react'
+import ResetPasswordCss from './ResetPassword.module.scss'
 
-import { useDispatch, useSelector } from "react-redux";
-import { LoadingSelector } from "../../redux/user/user.selector";
-import { Spinner } from "../../components/Spinner/Spinner";
-import { useForm } from "react-hook-form";
-import { passwordResetStart } from "../../redux/user/user.action";
+import { useDispatch, useSelector } from 'react-redux'
+import { LoadingSelector } from '../../redux/user/user.selector'
+import { Spinner } from '../../components/Spinner/Spinner'
+import { useForm } from 'react-hook-form'
+import { passwordResetStart } from '../../redux/user/user.action'
 
 const ResetPassword = () => {
-    const { register, handleSubmit } = useForm();
-    const dispatch = useDispatch();
-    const onSubmit = (data) => {
-        if (data.password === data.password_confirmation) {
-            dispatch(
-                passwordResetStart({
-                    ...data.password,
-                    email: "nbutt@gmail.com",
-                    token: "$2y$10$s65fL0FG50GolV2eSqzr0.0Nn4eS/IKIYIvIfRT6Ez0pwnR/Oruia",
-                })
-            );
-        } else {
-            alert("Password not match");
-        }
-    };
-    const loading = useSelector((state) => LoadingSelector(state));
-    return (
-        <div className={ResetPasswordCss.form}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <h3>Reset Password</h3>
-                <p>
-                    For the purpose of industry regulation, your details are
-                    required.
-                </p>
-                <label>New Password*</label>
-                <input
-                    type="password"
-                    placeholder="Enter Password"
-                    {...register("password")}
-                />
-                <label>New Password*</label>
-                <input
-                    type="password"
-                    placeholder="Enter Password Again"
-                    {...register("password_confirmation")}
-                />
-                <input
-                    type="submit"
-                    value="Reset Password"
-                    // onClick={() => {
-                    //   alert('Change Successfully')
-                    //   history.push('/')
-                    // }}
-                />
-            </form>
-            {loading ? <Spinner /> : null}
-        </div>
-    );
-};
+  const { register, handleSubmit } = useForm()
+  const dispatch = useDispatch()
+  const onSubmit = (data) => {
+    if (data.password === data.password_confirmation) {
+      dispatch(
+        passwordResetStart({
+          ...data.password,
+          email: 'nbutt@gmail.com',
+          token: '$2y$10$s65fL0FG50GolV2eSqzr0.0Nn4eS/IKIYIvIfRT6Ez0pwnR/Oruia',
+        })
+      )
+    } else {
+      alert('Password not match')
+    }
+  }
+  const loading = useSelector((state) => LoadingSelector(state))
+  return (
+    <div className={ResetPasswordCss.form}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h3>Reset Password</h3>
+        <p>
+          For the purpose of industry regulation, your details are required.
+        </p>
+        <label>New Password*</label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          {...register('password')}
+        />
+        <label>Confirm New Password*</label>
+        <input
+          type="password"
+          placeholder="Enter Password Again"
+          {...register('password_confirmation')}
+        />
+        <input
+          type="submit"
+          value="Reset Password"
+          // onClick={() => {
+          //   alert('Change Successfully')
+          //   history.push('/')
+          // }}
+        />
+      </form>
+      {loading ? <Spinner /> : null}
+    </div>
+  )
+}
 
-export default ResetPassword;
+export default ResetPassword
